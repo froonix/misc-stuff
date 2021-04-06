92f9f71987297d40b0336d2eceea354c330837af
Christian Schrötter <cs@fnx.li>
2021-04-06 08:16:17 +0200

    ...

--- android/afwall.sh
+++ android/afwall.sh
@@ -2,6 +2,7 @@
 #iptables -w -A afwall -p icmp -m owner ! --uid-owner 0-999999999 -j ACCEPT
 #ip6tables -w -A afwall -p icmpv6 -m owner ! --uid-owner 0-999999999 -j ACCEPT
 
+# XTRA-Download (A-GPS): ems.schroetter.co.at
 # Captive Portal Check: connectivitycheck.schroetter.co.at
 iptables -w -A afwall -d 46.232.249.13 -p tcp --dport 80 -m owner --uid-owner 1000 -j ACCEPT
 iptables -w -A afwall -d 46.232.249.13 -p tcp --dport 443 -m owner --uid-owner 1000 -j ACCEPT
